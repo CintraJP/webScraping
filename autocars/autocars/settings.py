db06a1a75cf0ecf34389dfbb03cb2dd95557eaa8
CintraJP <jpmcintra@gmail.com>
2023-10-29 22:04:24 -0300

    version 6

--- autocars/autocars/settings.py
+++ autocars/autocars/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '00e--3nzbt3b=4b$5%_ts^+ky0kat#bgj@=q@4_!q6&g7-kxci'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api-cars-ie4u.onrender.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'autocars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 #DATABASES = {
-#    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 #}
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
